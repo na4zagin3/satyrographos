9180541d946236e32d270f6b845b61543948f192
na4zagin3 <mrty.ityt.pt@gmail.com>
2018-11-11 01:22:14 +0900

    Replace Batteries with Core

--- src/satysfiDirs.ml
+++ src/satysfiDirs.ml
@@ -1,16 +1,16 @@
-open Batteries
+open Core
 
 let runtime_dirs () =
   if Sys.os_type = "Win32" then
-    match Sys.getenv_opt "SATYSFI_RUNTIME" with
+    match Sys.getenv "SATYSFI_RUNTIME" with
     | None    -> []
     | Some(s) -> [s]
   else
     ["/usr/local/share/satysfi"; "/usr/share/satysfi"]
 
 let home_dir () = if Sys.os_type = "Win32"
-  then Sys.getenv_opt "userprofile"
-  else Sys.getenv_opt "HOME"
+  then Sys.getenv "userprofile"
+  else Sys.getenv "HOME"
 
 let user_dir () =
-    Option.map (fun s -> Filename.concat s ".satysfi") (home_dir ())
+    Option.map ~f:(fun s -> Filename.concat s ".satysfi") (home_dir ())
