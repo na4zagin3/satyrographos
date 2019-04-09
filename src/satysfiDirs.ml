503aeef24c589dec97e6774bef7041006eb55497
na4zagin3 <mrty.ityt.pt@gmail.com>
2019-04-09 23:23:46 +0900

    Don’t fail even OPAM executive does not exist

--- src/satysfiDirs.ml
+++ src/satysfiDirs.ml
@@ -19,12 +19,26 @@ let is_runtime_dir dir =
   FileUtil.(test Is_dir) (Filename.concat dir "packages")
 
 let opam_share_dir () =
-  Unix.open_process_in "opam var share"
-  |> In_channel.input_all
-  |> String.strip
+  try
+    Unix.open_process_in "opam var share"
+    |> In_channel.input_all
+    |> String.strip
+    |> begin function
+      | "" -> None
+      | x -> Some x
+    end
+  with
+    Failure x ->
+      print_endline "Failed to get opam directory.";
+      print_endline x;
+      None
+
+let option_to_list = function
+  | Some x -> [x]
+  | None -> []
 
 let satysfi_dist_dir () =
-  let shares = [opam_share_dir (); "/usr/local/share"; "/usr/share"] in
+  let shares = option_to_list (opam_share_dir ()) @ ["/usr/local/share"; "/usr/share"] in
   let dist_dirs = List.map shares ~f:(fun d -> Filename.concat d "satysfi" |> (fun d -> Filename.concat d "dist")) in
   let rec f = function
     | [] -> failwith "Can't find SATySFi lib. Please install it."
