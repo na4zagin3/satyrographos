95914faf180a6010c63239b883b791d60f778a79
na4zagin3 <mrty.ityt.pt@gmail.com>
2020-10-03 06:20:18 +0000

    Sort the result of find in C collation

--- test/testcases/command_new__existing_directory.t
+++ test/testcases/command_new__existing_directory.t
@@ -4,5 +4,5 @@ Attempt to create a new library with the conflicting name
   Compatibility warning: You have opted in to use experimental features.
   test-lib already exists.
   [1]
-  $ LC_ALL=C find test-lib
+  $ find test-lib | LC_ALL=C sort
   test-lib
--- test/testcases/command_opam_build__doc_satysfi.ml
+++ test/testcases/command_opam_build__doc_satysfi.ml
@@ -44,7 +44,7 @@ build-doc:
 	@echo "Target: build-doc"
 	@echo 'Files under $$SATYSFI_RUNTIME'
 	@echo "=============================="
-	@cd "$(SATYSFI_RUNTIME)" ; LC_ALL=C find .
+	@cd "$(SATYSFI_RUNTIME)" ; find . | LC_ALL=C sort
 	@echo "=============================="
 |}
 
