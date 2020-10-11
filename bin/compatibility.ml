94dbc12ba8279d0c35b8747492a2193510d908ed
na4zagin3 <mrty.ityt.pt@gmail.com>
2020-10-11 22:35:15 +0900

    Output experimental feature warning to stderr

--- bin/compatibility.ml
+++ bin/compatibility.ml
@@ -3,8 +3,9 @@ open Core
 let optin () =
   match Sys.getenv "SATYROGRAPHOS_EXPERIMENTAL" with
   | Some "1" ->
-    Printf.printf "Compatibility warning: You have opted in to use experimental features.\n"
+    Printf.fprintf Out_channel.stderr "Compatibility warning: You have opted in to use experimental features.\n";
+    Out_channel.flush Out_channel.stderr
   | _ ->
-    Printf.printf "Compatibility warning: This is an experimental feature.\n";
-    Printf.printf "You have to opt in by setting env variable SATYROGRAPHOS_EXPERIMENTAL=1 to test this feature.\n";
+    Printf.fprintf Out_channel.stderr "Compatibility warning: This is an experimental feature.\n";
+    Printf.fprintf Out_channel.stderr "You have to opt in by setting env variable SATYROGRAPHOS_EXPERIMENTAL=1 to test this feature.\n";
     exit 1
