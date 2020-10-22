a2e0cecc2013f89c62ceb6ed4ca1ad02be5974c3
na4zagin3 <mrty.ityt.pt@gmail.com>
2020-10-22 10:48:33 +0000

    Move some functions in Opam to Build

---
+++ src/command/build.ml
@@ -0,0 +1,48 @@
+open Core
+open Satyrographos
+
+module P = Shexp_process
+
+let read_module ~outf ~verbose ~build_module ~buildscript_path =
+  let src_dir = Filename.dirname buildscript_path in
+  let p = BuildScript.read_module ~src_dir build_module in
+  if verbose
+  then begin Format.fprintf outf "Read library:@.";
+    [%sexp_of: Library.t] p |> Sexp.pp_hum outf;
+    Format.fprintf outf "@."
+  end;
+  (src_dir, p)
+
+let build_command ~satysfi_runtime = function
+      | "make" :: args ->
+        let command = P.run "make" (["SATYSFI_RUNTIME=" ^ satysfi_runtime] @ args) in
+        ProcessUtil.redirect_to_stdout ~prefix:"make" command
+      | "satysfi" :: args ->
+        RunSatysfi.run_satysfi ~satysfi_runtime args
+      | cmd -> failwithf "command %s is not yet supported" ([%sexp_of: string list] cmd |> Sexp.to_string) ()
+
+let run_build_commands ~outf ~verbose ~libraries ~workingDir ~env ~system_font_prefix ~autogen_libraries buildCommands =
+  let setup ~satysfi_dist =
+    Install.install satysfi_dist ~outf ~system_font_prefix ~autogen_libraries ~libraries ~verbose ~safe:true ~copy:false ~env ()
+  in
+  let commands satysfi_runtime = P.List.iter buildCommands ~f:(build_command ~satysfi_runtime) in
+  P.(chdir workingDir (RunSatysfi.with_env ~outf ~setup commands))
+
+let build ~outf ~verbose ~build_module ~buildscript_path ~system_font_prefix ~autogen_libraries ~env =
+  let src_dir, p = read_module ~outf ~verbose ~build_module ~buildscript_path in
+
+  match build_module with
+  | BuildScript.LibraryDoc build_module ->
+    let context = P.Context.create() in
+    let workingDir = Filename.concat src_dir build_module.workingDirectory in
+    let libraries = Library.Dependency.to_list p.dependencies |> Some in
+    let _, trace =
+      run_build_commands ~outf ~verbose ~workingDir ~libraries ~system_font_prefix ~autogen_libraries ~env build_module.build
+      |> P.Traced.eval_exn ~context in
+    if verbose
+    then begin Format.fprintf outf "Executed commands:@.";
+      Sexp.pp_hum_indent 2 Format.std_formatter trace;
+      Format.fprintf outf "@."
+    end
+  | BuildScript.Library _ ->
+    ()
--- src/command/opam.ml
+++ src/command/opam.ml
@@ -10,51 +10,13 @@ let library_dir prefix (buildscript: BuildScript.m) =
   let libdir = Filename.concat prefix "share/satysfi" in
   Filename.concat libdir (BuildScript.get_name buildscript)
 
-let read_module ~outf ~verbose ~build_module ~buildscript_path =
-  let src_dir = Filename.dirname buildscript_path in
-  let p = BuildScript.read_module ~src_dir build_module in
-  if verbose
-  then begin Format.fprintf outf "Read library:@.";
-    [%sexp_of: Library.t] p |> Sexp.pp_hum outf;
-    Format.fprintf outf "@."
-  end;
-  (src_dir, p)
-
-let run_build_commands ~outf ~verbose ~libraries ~workingDir ~env buildCommands =
-  let setup ~satysfi_dist =
-    Install.install satysfi_dist ~outf ~system_font_prefix:None ~autogen_libraries:[] ~libraries ~verbose ~safe:true ~copy:false ~env ()
-  in
-  let commands satysfi_runtime = P.List.iter buildCommands ~f:(function
-    | "make" :: args ->
-      let command = P.run "make" (["SATYSFI_RUNTIME=" ^ satysfi_runtime] @ args) in
-      ProcessUtil.redirect_to_stdout ~prefix:"make" command
-    | "satysfi" :: args ->
-      RunSatysfi.run_satysfi ~satysfi_runtime args
-    | cmd -> failwithf "command %s is not yet supported" ([%sexp_of: string list] cmd |> Sexp.to_string) ()
-  ) in
-  P.(chdir workingDir (RunSatysfi.with_env ~outf ~setup commands))
-
 let build_opam ~outf ~verbose ~prefix:_ ~build_module ~buildscript_path ~env =
-  let src_dir, p = read_module ~outf ~verbose ~build_module ~buildscript_path in
-
-  match build_module with
-  | BuildScript.LibraryDoc build_module ->
-    let context = Process.Context.create() in
-    let workingDir = Filename.concat src_dir build_module.workingDirectory in
-    let libraries = Library.Dependency.to_list p.dependencies |> Some in
-    let _, trace =
-      run_build_commands ~outf ~verbose ~workingDir ~libraries ~env build_module.build
-      |> P.Traced.eval_exn ~context in
-    if verbose
-    then begin Format.fprintf outf "Executed commands:@.";
-      Sexp.pp_hum_indent 2 Format.std_formatter trace;
-      Format.fprintf outf "@."
-    end
-  | BuildScript.Library _ ->
-    Format.fprintf outf "Building modules is not yet supported"
+  let system_font_prefix = None in
+  let autogen_libraries = [] in
+  Build.build ~outf ~verbose ~build_module ~buildscript_path ~system_font_prefix ~autogen_libraries ~env
 
 let install_opam ~outf ~verbose ~prefix ~build_module ~buildscript_path ~env:_ =
-  let _, p = read_module ~outf ~verbose ~build_module ~buildscript_path in
+  let _, p = Build.read_module ~outf ~verbose ~build_module ~buildscript_path in
   let dir = library_dir prefix build_module in
   Library.write_dir ~outf ~verbose ~symlink:false dir p
 
