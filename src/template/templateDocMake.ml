c9d934424c9f2bad9af11ae0340d698d263e2dcb
na4zagin3 <mrty.ityt.pt@gmail.com>
2020-10-27 12:32:10 +0000

    Update the template files

--- src/template/templateDocMake.ml
+++ src/template/templateDocMake.ml
@@ -4,6 +4,7 @@ let local_satyh_template =
 "local.satyh",
 {|% This is a file for local function/command definitions
 @require: code
+@require: math
 
 
 let-block ctx +frame content =
@@ -14,55 +15,58 @@ let-block ctx +frame content =
 let-block ctx +display-boxes content code =
   read-block (ctx |> set-paragraph-margin 12pt 0pt) '<+frame(content);>
     +++ read-block (ctx |> set-paragraph-margin 0pt 12pt) '<+code(code);>
+
+% Define a math command
+let-math \factorial x =
+  ${#x \mathpunct{\mathrm-token!(`!`)}}
 |}
 
 let main_saty_template =
 "main.saty",
 {|% This is the document file
 
-% Require stdjabook class package
+% Class package
 @require: stdjabook
 
-% Here are standard packages
+% Standard packages
 @require: annot
 @require: code
 @require: math
 @require: itemize
 
-% Here are third-party packages
+% Third-party packages
 @require: fss/fss
 @require: fss/fonts
 @require: fss/style
 
-% Here is a local package
-@import: ./local
+% Local package
+@import: local
 
 
 document (|
   title = {Test Document};
   author = {Your Name};
   show-title = true;
-  show-toc = true;
+  show-toc = false;
 |) '<
   +p {
     This template is for \SATySFi; 0.0.5.
-    As \SATySFi; is not yet mature,
+    As \SATySFi; is not yet murture,
     please be warned that \font-style[italic]{you may experience some breaking changes}.
   }
   +p {
-    Please join \href(`https://github.com/gfngfn/SATySFi/wiki/SATySFi-Wiki#satsysfi-slack`){\emph{SATySFi Slack}}!
+    There are online resources, so Please check out!
+    \listing{
+      * \href(`https://github.com/gfngfn/SATySFi/blob/master/demo/demo.saty`){`demo.saty`} is a brief introduction to \SATySFi;.
+      * Please join \href(`https://github.com/gfngfn/SATySFi/wiki/SATySFi-Wiki#satsysfi-slack`){\emph{SATySFi Slack}}!
+    }
   }
   +p {
-    `+p` block command represents a paragraph with indentation.
-  }
-  +pn {
-    On the other hand, `+pn` block command represents a paragraph without indentation.
+    As you see, `+p { ... }` represents a paragraph.
+    Technically speaking, `+p` is a block command applied to an inline text object `{ ... }`.
   }
   +p {
-    Block commands `+p` and `+pn` take a inline text `{ ... }` object.
-  }
-  +p {
-    An inline equation is represented by `${ ... }`. E.g., ${x^2 - x + 1}.
+    An inline equation is represented by a math object `${ ... }`. E.g., ${x^2 - x + 1}.
   }
   +p {
     Basic math commands resemble those in \LaTeX;. E.g., ${f: A \to \mathbb{R}}.
@@ -75,14 +79,55 @@ document (|
     E.g., \emph{abc} vs. \emph({abc});.
   });
   +p {
-    Block commands start with `+`; inline and math commands starts with `\`.
+    You can get a displayed equation by applying `\eqn` command to a math object. E.g.,
+    \eqn(${
+      \int_{M} d\alpha = \int_{\partial M}\alpha.
+    });
+    Similarly, you can get a code example with `\d-code` command.
+    \d-code(```
+    \eqn(${
+      \int_{M} d\alpha = \int_{\partial M}\alpha
+    });
+    ```);
   }
   +p {
-    `math` package provides enormous commands for equations.
+    `\math-list` takes a list of math objects.
+    \math-list[
+      ${\delta_{ij} = \cases![
+        (${1}, {${i = j}});
+        (${0}, {otherwise});
+      ]};
+      ${\epsilon_{a_{1}a_{2}\cdots a_{n}} =
+        \lower{\prod}{1\leq i\leq j\leq n}
+          \mathop{\mathrm{sgn}}\paren{a_{j} - a_{i}}
+      };
+    ];
+    `\align` takes a list of lists of math objects.
+    \align[
+      [ ${\pi};
+        ${=\paren{
+          \frac{2\sqrt{2}}{99^{2}}\upper{\lower{\sum}{n=0}}{\infty}
+            \frac{
+              \factorial{\paren{4n}}
+              \paren{1103 + 26390n}
+            }{
+              \paren{4^{n} 99^{n} \factorial{n}}^{4}
+            }
+          }^{-1}
+        };
+      ];
+      [ ${};
+        ${=\paren{
+          \int_{-\infty}^{\infty}
+          e^{
+            -x^2
+          }
+          \mathrm{d}x
+        }^{ 2 }
+        };
+      ];
+    ];
   }
-  +math(${
-    \int_{M} d\alpha = \int_{\partial M}\alpha
-  });
   +section{Sections} <
     +p {
       A new section is created by
@@ -90,7 +135,7 @@ document (|
     }
     +subsection{Subsection} <
       +p {
-        There's `+subsection` command too.
+        There’s `+subsection` command too.
       }
     >
   >
