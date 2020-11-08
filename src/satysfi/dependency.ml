4619079d5661247949f6827e88aea4f2c7057bed
na4zagin3 <mrty.ityt.pt@gmail.com>
2020-11-08 12:30:32 +0000

    gh-205: Support .satysfi-md as a source type of dependency graphs
    This commits adds .satysfi-md file as one source of dependency graphs.
    Meanwhile, lint subcommand still does not check dependencies of .satysfi-md files.
    See https://github.com/na4zagin3/satyrographos/issues/247.

--- src/satysfi/dependency.ml
+++ src/satysfi/dependency.ml
@@ -5,6 +5,7 @@ module Location = Satyrographos.Location
 type directive =
   | Import of string
   | Require of string
+  | MdDepends of string
 [@@deriving sexp, compare, hash, equal]
 
 type t = {
@@ -16,6 +17,7 @@ type t = {
 let render_directive = function
   | Import f -> sprintf "@import: %s" f
   | Require p -> sprintf "@require: %s" p
+  | MdDepends p -> sprintf "md depends %s" p
 
 let parse_directives =
   (* TODO Rewrite when multi-line comment is added to SATySFi *)
@@ -274,12 +276,67 @@ let%expect_test "parse_string_saty: directives followed by declarations" =
             ((rstart ((lnum 0) (cnum 0))) (rend ((lnum 0) (cnum 8))))))))
         (Import file))))) |}]
 
+let parse_satysfi_md_yojson ~path (json: Yojson.Safe.t) =
+  begin match json with
+    | `Assoc lvs ->
+      List.find_map lvs ~f:(function
+          | "depends", `List ds ->
+            List.filter_map ds ~f:(function
+                | `String s -> Some s
+                | _ ->
+                  failwithf
+                    "%s:\nDependency must be a list of strings but got %s@."
+                    path
+                    (Yojson.Safe.to_string (`List ds))
+                    ()
+              )
+            |> Option.some
+          | _ -> None
+        )
+      |> Option.map ~f:(fun ps ->
+          {
+            path = path;
+            directives =
+              let loc = {
+                Location.path;
+                range = None;
+              }
+              in
+              List.map ~f:(fun p -> loc, MdDepends p) ps
+          })
+    | _ ->
+      None
+  end
+  |> Option.value_exn ~message:(sprintf "File %s does not have depends field" path)
+
+let parse_satysfi_md_str ~path str =
+  Yojson.Safe.from_string str
+  |> parse_satysfi_md_yojson ~path
+
+let parse_satysfi_md_file ~path =
+  Yojson.Safe.from_file path
+  |> parse_satysfi_md_yojson ~path
+
+let%expect_test "parse_satysfi_md_str: valid" =
+  let path = "test.saty" in
+  parse_satysfi_md_str ~path {|{"depends":["mdja"]}|}
+  |> [%sexp_of: t]
+  |> Sexp.output_hum Out_channel.stdout;
+  [%expect{|
+    ((path test.saty)
+     (directives ((((path test.saty) (range ())) (MdDepends mdja))))) |}]
+
 let parse_file_result path =
-  Result.try_with (fun () ->
-      In_channel.read_all path
-    )
-  |> Result.map ~f:(match FilePath.get_extension path with
-      | _ -> parse_string_saty ~path)
+  match FilePath.get_extension path with
+  | "satysfi-md" ->
+    Result.try_with (fun () ->
+        parse_satysfi_md_file ~path)
+  | _ ->
+    Result.try_with (fun () ->
+        In_channel.read_all path
+      )
+    |> Result.map ~f:(match FilePath.get_extension path with
+        | _ -> parse_string_saty ~path)
 
 let referred_file_basenames ~package_root_dirs { path; directives; }=
   let basedir = FilePath.dirname path in
@@ -287,6 +344,7 @@ let referred_file_basenames ~package_root_dirs { path; directives; }=
       match d with
       | Import f ->
         loc, d, [FilePath.concat basedir f]
-      | Require p ->
+      | Require p
+      | MdDepends p ->
         loc, d, List.map package_root_dirs ~f:(fun rd -> FilePath.concat rd p)
     )
