8d66b9ea2dc0d8239469b700278202ff732d1d4e
na4zagin3 <mrty.ityt.pt@gmail.com>
2022-04-10 22:21:25 +0900

    Stop using process substitution

--- test/testcases/test-utils.sh
+++ test/testcases/test-utils.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 test_satysfi_pkgs () {
-    PKGS=$(SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos library-opam list 2>/dev/null )
-    command satysfi --version >/dev/null 2>&1 && [[ 0 -eq "$( sort <(printf "%s\n" "$@") <(echo "$PKGS") <(echo "$PKGS") | uniq -u | wc -l)" ]]
+    command satysfi --version >/dev/null 2>&1 || return $?
+    PKGS="$(mktemp)"
+    SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos library-opam list 2>/dev/null >"$PKGS"
+    [[ 0 -eq "$( printf "%s\n" "$@" | sort - <"$PKGS" <"$PKGS" | uniq -u | wc -l)" ]]
+
+    trap 'rm "$PKGS"' 0
 }
 
 test_omake () {
