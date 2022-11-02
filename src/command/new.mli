843912ee59d291d65157fce934185352effa5c2c
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-11-02 22:44:50 +0900

    add `new.mli`

---
+++ src/command/new.mli
@@ -0,0 +1,6 @@
+
+val template_map : (string * string) list Core.String.Map.t
+
+val template_descriptions : (string * string) list
+
+val create_project : string -> string option -> (string * string) list -> unit
